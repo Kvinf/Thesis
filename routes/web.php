5baac07eccd5d01b8e9dde9640fee3ff494ccf99
jonastheon7 <jonastheon7@gmail.com>
2024-05-03 14:53:00 +0700

    Add Dashboard

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,10 @@ Route::get('/signup', function () {
     return view('signup');
 })->name("signup");
 
+
+Route::get('/', function () {
+    return view('dashboard');
+})->name("dashboard");
+
 Route::post('register', [MsUserController::class, 'create'])->name('register');
 
