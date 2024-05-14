4c6abb2bef56cd26c57ce5f3fc7da210b2c6ce84
kevinkuandinata <kevin.kuandinata@binus.ac.id>
2024-05-14 15:51:29 +0700

    Commit Hover

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
     <link rel="stylesheet" href="../signup.css">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <!-- Latest compiled JavaScript -->
@@ -44,7 +45,7 @@
                     </div>
                     <div class="line"></div>
                     <div class="log_in">
-                        <p>Already have an account? <a class="linkItem" href="{{route("login")}}">Log in</a></p>
+                        <p>Already have an account? <a class="linkItem" href="{{ route('login') }}">Log in</a></p>
                     </div>
                 </form>
             </div>
